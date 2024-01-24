fe1ada85d14ad41f9cee1fe4dd429f6d1b9edc96
AmeerLalaDeveloper <laklaw100@gmail.com>
2024-01-24 16:53:58 +0200

    contact details ready to refactor code

--- src/pages/ProductDetails/components/Card/Card.tsx
+++ src/pages/ProductDetails/components/Card/Card.tsx
@@ -2,12 +2,12 @@ import { Product } from '@/localTypes/product.types';
 import styles from './Card.module.css';
 import ContactDetails from '../ContactDetails/ContactDetails';
 
-const Card: React.FC<Product> = ({ logo }) => {
+const Card: React.FC<Product> = ({ logo, email, link, linkedIn, phone }) => {
   return (
     <div className={styles.container}>
       <div className={styles.rightSide}>
         <img className={styles.logo} src={logo} alt="product logo" loading="lazy" />
-        <ContactDetails />
+        <ContactDetails email={email} phone={phone} linkedIn={linkedIn} link={link} />
       </div>
       <div className={styles.leftSide}></div>
     </div>
--- src/pages/ProductDetails/components/ContactDetails/ContactDetails.tsx
+++ src/pages/ProductDetails/components/ContactDetails/ContactDetails.tsx
@@ -1,24 +1,93 @@
-// import { useState } from 'react';
+import { useState } from 'react';
+import emailIcon from '@assets/icons/contact/email.png';
+import linkedinIcon from '@assets/icons/contact/linkedin.png';
+import telIcon from '@assets/icons/contact/tel.png';
+import webIcon from '@assets/icons/contact/web.png';
 import styles from './ContactDetails.module.css';
+import linkIcon from '@assets/icons/link.svg';
+import { IContactDetailsProps } from './ContactDetails.type';
 
-const ContactDetails = () => {
-  //   const [showDetails, setShowDetails] = useState(true);
-  const showDetails = false;
+const ContactDetails: React.FC<IContactDetailsProps> = ({
+  phone,
+  link,
+  linkedIn,
+  email,
+}) => {
+  console.log(phone, link);
+
+  const [showDetails, setShowDetails] = useState(false);
+  const toggleDetailsBtn = () => {
+    setShowDetails(!showDetails);
+  };
   return (
-    <div>
+    <div className={styles.container}>
       <div className={styles.linksContainer}>
-        <div className={styles.private}>
+        <div className={`${styles.private}  ${styles.list}`}>
           <div className={styles.detail}>
-            <img src={''} alt="phone" loading="lazy" />
-            <span className={styles.text && (!showDetails ? styles.hidden : '')}>
-              link
+            <img
+              className={`${styles.icon} ${showDetails ? '' : styles.lowOpacity}`}
+              src={telIcon}
+              alt="phone"
+              loading="lazy"
+            />
+            <span className={`${styles.name}  ${showDetails ? '' : styles.hidden}`}>
+              {phone}
             </span>
           </div>
+          <div className={styles.detail}>
+            <img
+              className={`${styles.icon} ${showDetails ? '' : styles.lowOpacity}`}
+              src={emailIcon}
+              alt="email"
+              loading="lazy"
+            />
+            <span className={`${styles.name}  ${showDetails ? '' : styles.hidden}`}>
+              {email}
+            </span>
+          </div>
+          <a href={link} target="_blank" className={styles.detail}>
+            <img
+              className={`${styles.icon} ${showDetails ? '' : styles.lowOpacity}`}
+              src={webIcon}
+              alt="email"
+              loading="lazy"
+            />
+            <span className={`${styles.name}  ${showDetails ? '' : styles.hidden}`}>
+              {link}
+              <img
+                src={linkIcon}
+                alt="link icon"
+                loading="lazy"
+                className={`${showDetails ? '' : styles.hidden}`}
+              />
+            </span>
+          </a>
         </div>
         <div className={styles.public}>
-          <div className={styles.detail}></div>
+          <a href={linkedIn} target="_blank" className={styles.detail}>
+            <img
+              className={`${styles.icon} ${showDetails ? '' : styles.lowOpacity}`}
+              src={linkedinIcon}
+              alt="email"
+              loading="lazy"
+            />
+            <span className={`${styles.name}  ${showDetails ? '' : styles.hidden}`}>
+              {linkedIn}
+              <img
+                className={`${showDetails ? '' : styles.hidden}`}
+                src={linkIcon}
+                alt="link icon"
+                loading="lazy"
+              />
+            </span>
+          </a>
         </div>
       </div>
+      {!showDetails && (
+        <button onClick={toggleDetailsBtn} className={styles.button}>
+          להציג פרטי תקשורת
+        </button>
+      )}
     </div>
   );
 };
